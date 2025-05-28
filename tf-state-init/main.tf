426a1d458a3870321efb75c32cbd6e6f5b4da3db
haneeshm <150122523+sagarmech98@users.noreply.github.com>
2025-05-29 00:10:40 +0530

    Add Azure storage container resource for Terraform state management

--- tf-state-init/main.tf
+++ tf-state-init/main.tf
@@ -11,3 +11,9 @@ resource "azurerm_storage_account" "tfstate" {
   account_tier             = "Standard"
   account_replication_type = "GRS"
 }
+
+resource "azurerm_storage_container" "tfstate" {
+  name                  = "roboshop-state-files"
+  storage_account_id    = azurerm_storage_account.tfstate.id
+  container_access_type = "private"
+}
