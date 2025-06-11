7e6aea5c17b9f8c139ad5dd95976c5b5739f5175
haneeshm <150122523+sagarmech98@users.noreply.github.com>
2025-06-11 11:55:01 +0530

    Fix variable references for rg_name and rg_location in tools-vm module

--- tools-setup/main.tf
+++ tools-setup/main.tf
@@ -1,8 +1,8 @@
 module "tools-vm" {
   for_each    = var.tools
   source      = "./vm"
-  rg_name     = "var.rg_name"
-  rg_location = "var.rg_location"
+  rg_name     = var.rg_name
+  rg_location = var.rg_location
   name = each.key
   vm_size = each.value["vm_size"]
 }
