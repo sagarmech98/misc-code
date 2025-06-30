9a915b81a8bd127ae8b2ae755dfc5fbf2cdde48e
haneeshm <150122523+sagarmech98@users.noreply.github.com>
2025-06-30 17:34:19 +0530

    Add frontend values to variables for RoboShop application

--- vault-secrets/variables.tf
+++ vault-secrets/variables.tf
@@ -20,6 +20,16 @@ variable "values" {
         foo = "bar"
       }
     }
+    frontend = {
+      secret = "roboshop-dev"
+      value = {
+        catalogue_url = "http://catalogue-dev.vidyapractice.shop:8080/"
+        user_url      = "http://user-dev.vidyapractice.shop:8080/"
+        cart_url      = "http://cart-dev.vidyapractice.shop:8080/"
+        payment_url   = "http://payment-dev.vidyapractice.shop:8080/"
+        shipping_url  = "http://shipping-dev.vidyapractice.shop:8080/"
+      }
+    }
     ssh = {
       secret = "roboshop-infra"
       value = {
