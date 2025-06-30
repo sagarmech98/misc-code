9d6257590cbaf274d4ad1e58d23d916c18cb8dbf
haneeshm <150122523+sagarmech98@users.noreply.github.com>
2025-06-30 15:02:21 +0530

    Add dependency on vault_mount for vault_kv_secret_v2 resource

--- vault-secrets/main.tf
+++ vault-secrets/main.tf
@@ -7,8 +7,9 @@ resource "vault_mount" "kv" {
 }
 
 resource "vault_kv_secret_v2" "values" {
-  for_each  = var.values
-  mount     = each.value["secret"]
-  name      = each.key
-  data_json = jsonencode(each.value["value"])
+  depends_on = [vault_mount.kv]
+  for_each   = var.values
+  mount      = each.value["secret"]
+  name       = each.key
+  data_json  = jsonencode(each.value["value"])
 }
