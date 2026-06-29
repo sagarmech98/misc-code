bb6b32243dbb038f471dc5983cad7b6be096ae99
haneeshm <150122523+sagarmech98@users.noreply.github.com>
2026-06-29 17:20:56 +0530

    changed rg name

--- tools-setup-spotdiscount/provider.tf
+++ tools-setup-spotdiscount/provider.tf
@@ -7,7 +7,7 @@ provider "azurerm" {
 terraform {
   backend "azurerm" {
     subscription_id      = "72129bc2-1be7-4c6e-971e-9375ebd6c232"
-    resource_group_name  = "Project"
+    resource_group_name  = "Project-Robo"
     storage_account_name = "stgdevtfstates"
     container_name       = "roboshop-state-files"
     key                  = "tools.terraform.tfstate"
